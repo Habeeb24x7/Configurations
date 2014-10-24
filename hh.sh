ddb0cd8642b985249d1cfe09365e8d75d2581d3e
Habeeb24x7 <Habeeb24x7@users.noreply.github.com>
2014-10-24 23:08:35 +1100

    Update hh.sh

--- hh.sh
+++ hh.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 mv /tmp/tomcat/apache-tomcat-7.0.56 /opt/tomcat
-cp -r /tmp/zookeeper/zookeeper-3.4.6 /opt/zookeeper
+cp -r /tmp/zookeeper/zookeeper-3.4.6/* /opt/zookeeper
 mv /tmp/solr/solr-4.10.1 /opt/solr_install
 cp -r /opt/solr_install/example/solr/* /opt/solr_home
 cp /opt/solr_install/dist/solr-*.war /opt/solr_home/war/solr.war
 rm -rf /opt/solr_home/collection1
 cp –r /opt/solr_install/example/lib/ext/* /opt/tomcat/lib/
-cp –r /opt/solr_install/example/resources/log4j.properties /opt/tomcat/lib/
+cp /opt/solr_install/example/resources/log4j.properties /opt/tomcat/lib/
 /opt/tomcat/bin/startup.sh
 sleep 10s
 /opt/tomcat/bin/shutdown.sh
 mv /tmp/hhconfig/solr.xml /opt/tomcat/conf/Catalina/localhost/solr.xml
 /opt/tomcat/bin/startup.sh
-rm -rf /tmp/zookeeper/zookeeper-3.4.6
+#rm -rf /tmp/zookeeper/zookeeper-3.4.6
